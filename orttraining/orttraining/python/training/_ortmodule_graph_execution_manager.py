c925e7427987d4d46a7657867f32c3b6cbbc6f5d
Satya Jandhyala <satya.k.jandhyala@gmail.com>
2021-04-08 20:47:18 +0000

    Added graph_transformer_config and propagate_cast_ops_level option.

--- orttraining/orttraining/python/training/_ortmodule_graph_execution_manager.py
+++ orttraining/orttraining/python/training/_ortmodule_graph_execution_manager.py
@@ -80,6 +80,9 @@ class GraphExecutionManager(ABC):
         self._save_onnx = False
         self._save_onnx_prefix = ''
 
+        # Graph transformer config
+        self._propagate_cast_ops_level = 0
+
         # Value can be either torch.onnx.TrainingMode.TRAININGor torch.onnx.TrainingMode.EVAL
         # To be instantiated in the concrete implementation of GraphExecutionManager
         self._export_mode = None
@@ -299,6 +302,8 @@ class GraphExecutionManager(ABC):
         grad_builder_config.initializer_names_to_train = initializer_names_to_train
         grad_builder_config.input_names_require_grad = self._input_names_require_grad
         grad_builder_config.build_gradient_graph = training
+        grad_builder_config.graph_transformer_config = C.GraphTransformerConfiguration()
+        grad_builder_config.graph_transformer_config.propagate_cast_ops_level = self._propagate_cast_ops_level
         self._graph_builder = C.OrtModuleGraphBuilder()
         self._graph_builder.initialize(
             self._onnx_model.SerializeToString(), grad_builder_config)
